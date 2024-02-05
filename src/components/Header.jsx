a67b9d7330cf14e4c6a1d7d4506cb83a6b80df27
marcelorpc <marcelorpchaves@gmail.com>
2024-02-05 08:59:03 -0300

    finish header full responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,25 @@ import smartphoneIllustration from '../assets/smartphone-illustration.svg'
 
 export function Header() {
   return (
-    <header>
-      <nav>
-        <img src={workitLogo} alt="Workit Logo" />
-        <a href="#site">
-          Apply for access
-        </a>
-      </nav>
+    <div className={styles.baseBg}>
+      <header>
+        <nav>
+          <img src={workitLogo} alt="Workit Logo" />
+          <a href="#site">
+            Apply for access
+          </a>
+        </nav>
 
-      <div className={styles.mainTitle}>
-        <h1>Data <span>tailored</span> to your needs.</h1>
+        <div className={styles.mainTitle}>
+          <h1>Data <span>tailored</span> to your needs.</h1>
 
-        <button className={styles.mainButton}>Learn more</button>
-      </div>
+          <button className={styles.mainButton}>Learn more</button>
+        </div>
 
-      <img className={styles.smartphone} src={smartphoneIllustration} alt="an illustration of a smartphone" />
-    </header>
+        <div className={styles.smartphoneWrapper}>
+          <img className={styles.smartphone} src={smartphoneIllustration} alt="an illustration of a smartphone" />
+        </div>
+      </header>
+    </div>
   )
 }
