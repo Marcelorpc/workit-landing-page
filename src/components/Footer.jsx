5ee5944b9c39fb635a861bfc4ae250bea67a72d8
marcelorpc <marcelorpchaves@gmail.com>
2024-02-06 09:55:10 -0300

    finish footer full responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,36 @@
 import styles from './Footer.module.css'
 
+import workitLogo2 from '../assets/workit-logo-2.svg'
+import facebookLogo from '../assets/facebook-logo.svg'
+import twitterLogo from '../assets/twitter-logo.svg'
+import instagramLogo from '../assets/instagram-logo.svg'
+
 export function Footer() {
   return (
-    <p></p>
+    <footer>
+      <img src={workitLogo2} alt="Logo da Workit" />
+
+      <nav>
+        <ul className={styles.linksList}>
+          <li>
+            <a href="#site" target='_blank'>
+              <img src={facebookLogo} alt="Logo do facebook" />
+            </a>
+          </li>
+
+          <li>
+            <a href="#site" target='_blank'>
+              <img src={twitterLogo} alt="Logo do twitter" />
+            </a>
+          </li>
+
+          <li>
+            <a href="#site" target='_blank'>
+              <img src={instagramLogo} alt="Logo do instagram" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
   )
 }
