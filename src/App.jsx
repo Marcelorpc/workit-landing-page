5b267eddb29c497418a44d6eeb4c6d1f71055ba5
marcelorpc <marcelorpchaves@gmail.com>
2024-02-07 20:20:32 -0300

    add Content component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './global.css'
 import styles from './App.module.css'
 
 import { Header } from './components/Header'
+import { Content } from './components/Content'
 import { Footer } from './components/Footer'
 
 function App() {
   return (
     <div>
       <Header />
+      <Content />
       <Footer />
     </div>
   )
